76d2b706e6d95bea79eb91d3e0317adf332095b8
dvmnum <bolotskikh.vladimir97@mail.ru>
2024-04-10 21:06:15 +0300

    sprint-6/step-1 fixed

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,3 +1,17 @@
+Cypress.Commands.add('have_text', (sel, text) => {
+  cy.get(sel).should('have.text', text)
+})
+
+Cypress.Commands.add('contain_html', (sel, html) => {
+  cy.get(sel).should('contain.html', html)
+})
+
+Cypress.Commands.add('loc', (location) => {
+  cy.location().should((loc) => {
+    expect(loc.hash).to.eq(location)
+  })
+})
+
 describe('template spec', () => {
   let email = 'dvmnumlol@yandex.ru'
   let password = '1407'
@@ -13,6 +27,10 @@ describe('template spec', () => {
       JSON.stringify("test-refreshToken")
     );
     cy.setCookie('accessToken', 'test-accessToken')
+
+    cy.get('[data-testid=constructor_top]').as('top')
+    cy.get('[data-testid=constructor_middle]').as('middle')
+    cy.get('[data-testid=constructor_bottom]').as('bottom')
   });
 
   afterEach(() => {
@@ -22,8 +40,8 @@ describe('template spec', () => {
 
   it('modal', () => {
     cy.get('.listElement:nth-child(1) ul a:nth-child(1)').click()
-    cy.get('#modal .text.text_type_main-large').should('have.text', 'Детали ингредиента')
-    cy.get('[data-testid=ingredient_name]').should('have.text', 'Краторная булка N-200i')
+    cy.have_text('#modal .text.text_type_main-large', 'Детали ингредиента')
+    cy.have_text('[data-testid=ingredient_name]', 'Краторная булка N-200i')
 
     cy.get('#modal svg').click()
     cy.get('#modal').should('have.html', '')
@@ -31,38 +49,34 @@ describe('template spec', () => {
 
   it('drag', () => {
     cy.get('.listElement:nth-child(1) ul a:nth-child(1)').trigger('dragstart')
-    cy.get('[data-testid=constructor_top]').trigger('drop')
-    cy.get('[data-testid=constructor_top]').should('contain.html', 'constructor-element__image')
-    cy.get('[data-testid=constructor_bottom]').should('contain.html', 'constructor-element__image')
+    cy.get('@top').trigger('drop')
+    cy.contain_html('@top', 'constructor-element__image')
+    cy.contain_html('@bottom', 'constructor-element__image')
     
     cy.get('.listElement:nth-child(2) ul a:nth-child(1)').trigger('dragstart')
-    cy.get('[data-testid=constructor_middle]').trigger('drop')
-    cy.get('[data-testid=constructor_middle] [data-testid=dropped_item]:nth-child(1)').should('exist')
+    cy.get('@middle').trigger('drop')
+    cy.get('@middle').get('[data-testid=dropped_item]:nth-child(1)').should('exist')
     
     cy.get('.listElement:nth-child(3) ul a:nth-child(1)').trigger('dragstart')
-    cy.get('[data-testid=constructor_middle]').trigger('drop')
-    cy.get('[data-testid=constructor_middle] [data-testid=dropped_item]:nth-child(2)').should('exist')
+    cy.get('@middle').trigger('drop')
+    cy.get('@middle').get('[data-testid=dropped_item]:nth-child(2)').should('exist')
   })
 
   it('order', () => {
     cy.get('.listElement:nth-child(1) ul a:nth-child(1)').trigger('dragstart')
-    cy.get('[data-testid=constructor_top]').trigger('drop')
-    cy.get('[data-testid=constructor_top]').should('contain.html', 'constructor-element__image')
-    cy.get('[data-testid=constructor_bottom]').should('contain.html', 'constructor-element__image')
+    cy.get('@top').trigger('drop')
+    cy.contain_html('@top', 'constructor-element__image')
+    cy.contain_html('@bottom', 'constructor-element__image')
 
-    cy.get('.BurgerConstructor_order__M9EAb').click()
+    cy.get('[data-testid=order_button]').click()
 
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq('/login')
-    })
+    cy.loc('#/login')
 
     cy.get('[data-testid=email_input]').type(`${email}{enter}`)
     cy.get('[data-testid=password_input]').type(`${password}{enter}`)
     cy.get('[data-testid=submit_button]').click()
 
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq('/')
-    })
+    cy.loc('#/')
 
     cy.get('[data-testid=order_button]').click()
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,18 @@ import reportWebVitals from './reportWebVitals';
 import App from './components/App/App';
 import { Provider } from "react-redux";
 import { store } from './services/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
